5f37f41a74aa752b0890723c8d5eee80f7a620aa
Aimen Taoussi <aitaouss@e3r8p1.1337.ma>
2024-04-19 01:29:33 +0100

    three push

--- cpp00/ex01/phonebook.hpp
+++ cpp00/ex01/phonebook.hpp
@@ -3,37 +3,20 @@
 
 # include <iostream>
 # include <string>
-# include "contact.hpp"
+
+class Contact {
+    public:
+    std::string first_name;
+    std::string last_name;
+    std::string nickname;
+    std::string darksecret;
+    std::string phone_number;
+};
 
 class PhoneBook {
-public:
+    public:
     Contact contacts[8];
     int contact_count;
-
-public:
-    PhoneBook() {
-        contact_count = 0;
-    }
-
-    void add_contact(const Contact& contact) {
-        contacts[contact_count] = contact;
-        contact_count++;
-
-        std::cout << "Contact added successfully." << std::endl;
-    }
-    // display all the saved contact
-    void display_contact(int index) {
-        if (index < 0 || index >= contact_count) {
-            std::cout << "Invalid index. Please try again." << std::endl;
-            return;
-        }
-        Contact contact = contacts[index];
-        std::cout << "First name: " << contact.first_name << std::endl;
-        std::cout << "Last name: " << contact.last_name << std::endl;
-        std::cout << "Nickname: " << contact.nickname << std::endl;
-        std::cout << "Phone number: " << contact.phone_number << std::endl;
-        std::cout << "Darkest secret: " << contact.darkest_secret << std::endl;
-    }
 };
 
 #endif
