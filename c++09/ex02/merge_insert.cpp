1c50a2e3ae355f2f24f8a084626f4ff20c57d7d8
Redx-at <taoussi.aimen@gmail.com>
2024-12-17 13:32:50 +0100

    c++09 ex02 binary Search

--- c++09/ex02/merge_insert.cpp
+++ c++09/ex02/merge_insert.cpp
@@ -6,13 +6,13 @@
 /*   By: aitaouss <aitaouss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 17:59:44 by aitaouss          #+#    #+#             */
-/*   Updated: 2024/12/16 22:48:43 by aitaouss         ###   ########.fr       */
+/*   Updated: 2024/12/17 12:43:26 by aitaouss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
 
-void    SortPairsRecursively(std::vector<std::pair<int, int> >& Vpairs, int i) {
+void    SortPairsRecursively(std::vector<std::pair<int, int> >& Vpairs, size_t i) {
     if (i >= Vpairs.size() - 1) {
         return ;
     }
@@ -47,13 +47,15 @@ int main() {
     
     std::pair<int, int> pairs;
     std::vector<std::pair<int, int> >  Vpairs;
-    for (int i = 0; i < mergeSort.size(); i += 2) {
+    for (size_t i = 0; i < mergeSort.size(); i += 2) {
         pairs = std::make_pair(mergeSort[i], mergeSort[i + 1]);
         std::cout << "{" << pairs.first << " , " << pairs.second << "}" << std::endl;
     }
     std::cout << "----- After the sort ------" << std::endl;
-    for (int i = 0; i < mergeSort.size(); i += 2) {
+    for (size_t i = 0; i < mergeSort.size(); i += 2) {
         if (i < mergeSort.size()) {
+
+            
             pairs = std::make_pair(mergeSort[i], mergeSort[i + 1]);
             if (pairs.first < pairs.second) {
                 std::swap(pairs.first, pairs.second);
@@ -63,15 +65,36 @@ int main() {
         }
     }
     std::cout << "------ OUtput the Vpair ------" << std::endl; 
-    for (int i = 0; i < Vpairs.size(); i++) {
+    for (size_t i = 0; i < Vpairs.size(); i++) {
         std::cout << Vpairs[i].first << " | " << Vpairs[i].second << std::endl;
     }
     std::cout << "----- SIZE ------" << std::endl;
     std::cout << "Size : " << Vpairs.size() << std::endl;
     std::cout << "----- After the Vect ------" << std::endl;
     SortPairsRecursively(Vpairs, 0);
-    for (int i = 0; i < Vpairs.size(); i++) {
+    for (size_t i = 0; i < Vpairs.size(); i++) {
         std::cout << Vpairs[i].first << " | " << Vpairs[i].second << std::endl;
     }
-    // Step 4 in the tldraw
+
+    std::vector<int>    MainChain;
+    std::vector<int>    PendChain;
+    
+    for (size_t i = 0; i < Vpairs.size(); i++) {
+        MainChain.push_back(Vpairs[i].first);    
+        PendChain.push_back(Vpairs[i].second);    
+    }
+    
+    std::vector<int>::iterator it = MainChain.begin();
+    if (MainChain[0] >= PendChain[0]) {
+        MainChain.insert(it , PendChain[0]);
+        PendChain.erase(PendChain.begin());
+    }
+    std::cout << "----------- Main Chain -----------" << std::endl;
+    for (size_t i = 0; i < MainChain.size(); i++) {
+        std::cout << MainChain[i] << std::endl;
+    }
+    std::cout << "----------- Pend Chain -----------" << std::endl;
+    for (size_t i = 0; i < PendChain.size(); i++) {
+        std::cout << PendChain[i] << std::endl;
+    }
 }
